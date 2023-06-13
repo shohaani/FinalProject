803a86fd3c26717bc0aaf401101ef1e7e3d277c8
Karla Castro <castromorenokarla0@gmail.com>
2023-06-13 15:26:27 -0400

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by scholar on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
